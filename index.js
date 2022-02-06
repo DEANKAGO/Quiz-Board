d8427378b485bc8d7139781ec5881b204b15946e
martin <martokhago@gmail.com>
2022-02-06 18:30:33 +0300

    js: js-function - Created js function to get form

---
+++ index.js
@@ -0,0 +1,8 @@
+var form = document.getElementById("quiz-form");
+var btn = document.getElementById("btn");
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+
+  console.log(e.div)
+
+});
